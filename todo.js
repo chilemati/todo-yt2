3d7b4a0b81b42453c26e4e362d5301cada7cc1af
chilemati <amadichile007@gmail.com>
2024-08-10 18:29:27 +0100

    first commit

--- todo.js
+++ todo.js
@@ -1,187 +1,195 @@
-// get month from date object
-let d = new Date();
-//  console.log(d.toDateString()[0])
-let date = d.toDateString();
+//? selecting element
 
-// console.log(date.split(' ')[3]);
-// target nav h1
-let navH1 = document.querySelector('.nav h1');
-navH1.textContent += date.split(' ')[1] + ', ' + groupBySpace(date,2) + " " + date.split(' ')[3];
-// console.log(navH1);
-// 
+// by id
 
+/* 
+  let body = document.getElementById('idValue');
+*/
 
-// target nav button
-let navButton = document.querySelector('.nav button');
-let getBody = document.querySelector('body');
-// console.log(navButton);
+let bodyById = document.getElementById('body');
 
-navButton.addEventListener("click", (e)=> {
-e.preventDefault(e);
-getBody.classList.toggle('darkTheme');
-if(getBody.className === "darkTheme"){
-    navButton.textContent="Light Theme";
-    showDiag('Your are now in Dark Mode');
-}
-else{
-    navButton.textContent="Dark Theme";
-    showDiag('Back to Light Mode');
+// console.log(bodyById)
 
-}
-// console.log(getBody.className);
+// by querySelector
+//! when using the query select, target element just like in  external css
+let bodyq = document.querySelector('#body .nav');
+
+console.log(bodyq)
+
+// by querySelectorAll
+
+let allLi = document.querySelectorAll('.tasks li');
+
+allLi.forEach(li=> {
+  console.log(li)
 })
-//
+console.log(allLi)
 
+// ? dom navigation
 
-// console.log(todos);
-/*
-STEPS TO ACTIVATE A SEARCH BAR:
-// get the search input
-// add a keyup event listener to the search input
-// in the event listener: {
-    // get the element holding each task or the li. not the ol
-// convert the values from the input to lowercase
-    // use foreach to loop through the html array collection
-    // in the foreach: {
-        // get the firstelementchild of the text tag and convert to lowercase
-        // use indexOf() to check of the values from search input exist in the text tags
-        // if true, show the tasks else hide tasks
-    }
-}
+let navH1 = document.querySelector('.nav h1');
+
+console.log(navH1.parentElement)
+console.log(navH1.parentElement.firstElementChild)
+console.log(navH1.parentElement.lastElementChild)
+console.log(navH1.nextElementSibling)
+console.log(navH1.nextElementSibling.previousElementSibling)
+
+//? getting and changing text
+
+/* 
+ get text
+
+ variableName.textContent;
+
+ set text
+
+ variableName.textContent = 'value';
 
 */
 
-let getSearch= document.querySelector('.search  input');
+let nav = navH1.parentElement;
+// gettting text
+console.log(nav.textContent)
+console.log(nav.innerText)
+console.log(nav.innerHTML)
 
-// console.log(taskLi);
-getSearch.addEventListener('keyup', ()=> {
-    let searchValue= getSearch.value;
-    let lowerValue= searchValue.toLowerCase();
-    let taskLi= document.querySelectorAll('.tasks li');
-    // console.log(lowerValue);
-    taskLi.forEach(x => {
-        // console.log(x.firstElementChild.textContent);
-        if(x.firstElementChild.textContent.toLocaleLowerCase().indexOf(lowerValue) != -1){
-            // console.log('true');
-            x.style.display='flex';
-        }
-        else{
-            // console.log('false');
-            x.style.display='none';
-        }
-        
-    });
-})
+// setting text
+// a+=b expand  a= a + b;
+navH1.textContent += ' App'
 
+// setting attribute
 
-//  adding tasks
+/* 
+ variableName.setAttribute('attributeName','attributeValue');
+*/
 
-let getForm = document.querySelector('.addTask');
+navH1.setAttribute('title','Todo List App')
 
-function addNewTasks() {
-    // create tags
-let newLi= document.createElement('li');
-let newSpan1= document.createElement('span');
-let newSpan2= document.createElement('span');
-let getTaskOl = document.querySelector('.tasks ol');
-// add class to tags
-newSpan1.classList.add('task');
-newSpan2.classList.add('delete');
-newLi.classList.add('li');
-// append
-newLi.appendChild(newSpan1);
-newLi.appendChild(newSpan2);
-// get addTask input and append newLi
-let getAddTask = document.querySelector('.addTask input');
+// remove attribute
 
+nav.setAttribute('class','nav NAV')
 
- let addTaskInput= getAddTask.value;
+// adding css
 
- if(String(addTaskInput).length%20 == 0) {
-    showDiag('Task need to be short');
-   
- }
- else{
-    newSpan1.style.textTransform='capitalize';
-    newSpan1.innerHTML= addTaskInput;
-    newSpan2.innerHTML= 'Delete';
-    getTaskOl.appendChild(newLi);
-    getForm.reset();
-    showDiag('New task added Successfullyyyyyyyyyyyyyyyyyyyyyyyyyy!')
- }
+// inline css
 
+/* 
+ color: red;
+ font-size: 2px; | fontSize = '2px';
 
- 
+variableName.style.cssProperty= 'value' 
+
+*/
+
+// navH1.style.color = 'red';
+// navH1.style.fontSize = '10px'
+
+// internal css
+// when using cssText, write your css rule like you do in your external css
+
+// navH1.style.cssText = `
+//  color: red;
+//  font-size: 1.5rem;
+//  font-style: italic;
+// `
+
+// external css
+
+// navH1.setAttribute('class','navH1');
+
+navH1.classList.add('h1Style');
+navH1.classList.remove('h1Style');
+
+// create element
+
+/* 
+ let p = document.createElement('p');
+*/
+
+let li = document.createElement('li');
+let spanTask = document.createElement('span');
+let spanDelete = document.createElement('span');
+
+// add attributes
+li.setAttribute('class','li li4');
+spanTask.setAttribute('class','task');
+spanDelete.setAttribute('class','delete');
+
+// add text
+spanTask.textContent = 'New Task';
+spanDelete.textContent = 'Delete';
+
+// rearrange: append or prepend
+li.append(spanTask)
+li.append(spanDelete)
+
+// add to dom
+let taskOl = document.querySelector('.tasks ol');
+
+taskOl.append(li)
+
+//? replacing an element
+
+// replace li1 task span to a tag
+
+/* 
+  select li
+  create a and add text and attribute
+  add to dom
+*/
+
+let li1 = document.querySelector('.tasks .li1'); 
+let li2 = document.querySelector('.tasks .li2'); 
+
+let a = document.createElement('a');
+
+// add text
+a.textContent = 'Learn React Js';
+
+// add attribute
+
+a.setAttribute('href','http://google.com')
+a.setAttribute('target','_blank');
+
+// add to dom
+
+li1.replaceChild(a,li1.childNodes[1]);
+
+console.log(li1)
+// removing an element;
+
+/* 
+  variableName.remove();
+*/
+
+li2.remove();
+
+//? htmll event
+
+function handleClick(e) {
+  alert('You Clicked Me');
+  console.log()
 }
 
-getForm.addEventListener('submit', (e)=> {
-    e.preventDefault(e);
-addNewTasks();
-
-
-})
-
-
-
-
-// delect tasks
-
-// in order to delete added task, add the event listener on the task section element and not on the tag containing the task to be deleted
-
-let getDelClass = document.querySelectorAll('.tasks');
-
-
-//  console.log(getDelClass);
-
- getDelClass.forEach(x => {
-   x.addEventListener('click', (e) => {
-    // console.log(e.target.className);
-    // console.log(x);
-
-    if(e.target.className == 'delete') {
-        e.target.parentElement.style.display='none';
-        showDiag('Task Deleted Successfully!');
-    }
-   })
- });
-
- let getDiag = document.querySelector('.diag');
-    getDiag.style.display='none';
- function showDiag(a){
-    //  diag
-    let getDiagText=getDiag.querySelector('.info');
-
-    let getClose=getDiag.querySelector('.close');
-    getDiag.style.display='flex';
-    getDiag.classList.add('diagState');
-    getDiagText.value=a;
-
-    getClose.addEventListener('click', ()=> {
-    getDiag.classList.remove('diagStage');
-   let getDiagForm= document.querySelector('.diagReset').reset();
-    getDiag.style.display='none';
-
-})
- }
-
- 
-
-
-// functions
-function groupBySpace(a,b) {
-    a= Array.from(a);
-    let h='', result = [];
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] != ' ') {
-            h += a[i];
-        } else {
-            result.push(h);
-            h = '';
-            
-        }
-        // console.log(a[i])
-    }
-    (h != ' ') ? result.push(h) : ' ';
-
-    return b? result[b]: result.join(' ');
+function test() {
+  alert('a ran');
 }
+
+// calling or invoking a function
+
+// test();
+
+//? dom eveent
+
+/* 
+  variable.addEventListener('eventType',function);
+*/
+
+bodyById.addEventListener('click',(e)=> {
+  console.log(e.target)
+})
+
+li1.addEventListener('mouseleave',(e)=> {
+  alert('Mouse left')
+})
